ed18314c4218064db599a24cae9ca1881469630e
Johnny Mohseni <jmohseni@gmail.com>
2016-05-27 12:18:11 -0400

    first attempt at the most basic integration with TravisCI

--- server/boot/root.js
+++ server/boot/root.js
@@ -1,6 +1,9 @@
-module.exports = function(server) {
-  // Install a `/` route that returns server status
-  var router = server.loopback.Router();
-  router.get('/', server.loopback.status());
-  server.use(router);
-};
+// Commented out so the root route is not served via this file.  This allows
+// the static content from the /client folder to be used.
+//
+// module.exports = function(server) {
+//   // Install a `/` route that returns server status
+//   var router = server.loopback.Router();
+//   router.get('/', server.loopback.status());
+//   server.use(router);
+// };
