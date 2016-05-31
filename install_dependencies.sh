1eaef6fca9d5fdb7ab0bea6a32d2324ee73c2a10
Johnny Mohseni <jmohseni@gmail.com>
2016-05-31 14:53:32 -0400

    first attempt at AWS Code Deploy integration

---
+++ install_dependencies.sh
@@ -0,0 +1,6 @@
+cd /webapps/stage
+curl --silent --location https://rpm.nodesource.com/setup_4.x | bash -
+yum -y install node_js
+npm install npm -g
+npm install strongloop -g
+npm install
---
+++ start_app.sh
@@ -0,0 +1,2 @@
+cd /webapps/stage
+slc start
