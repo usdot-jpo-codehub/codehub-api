ce51005425baf2acfa9a82f15d56ef166c82a419
Johnny Mohseni <jmohseni@gmail.com>
2016-06-01 18:08:31 -0400

    bundle property needs to be present so AWS CodeDeploy understands how to inspect the package it downloads

--- start_app.sh
+++ start_app.sh
@@ -1,4 +1,4 @@
 cd /opt/heimdall
 /sbin/initctl start strong-pm
 sl-pmctl create heimdall-webapp-service
-slc deploy -s heimdall-webapp-service http://127.0.0.18071 ./heimdall-webapp-1.0.0.tgz
+slc deploy -s heimdall-webapp-service http://127.0.0.1:8071 ./heimdall-webapp-1.0.0.tgz
