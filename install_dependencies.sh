7ef52508f359760cafaeec3f71086cce1c92553e
Johnny Mohseni <jmohseni@gmail.com>
2016-06-02 21:48:49 -0400

    adding jspm_packaages folder so we avoid the github api timeout from travis

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -11,9 +11,7 @@ npm install -g strongloop
 # We will need to figure out a better way to handle this.
 cd client
 npm install -g gulp
-npm install -g jspm
 npm install
-jspm install -y
 gulp build
 
 # Install and Pack the Node/Loopback Dependencies
