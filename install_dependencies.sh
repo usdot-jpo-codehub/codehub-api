5c0b42eab0deed730306b472c0ff19092b80e5df
Johnny Mohseni <jmohseni@gmail.com>
2016-06-08 10:05:54 -0400

    doing all the installing on the CI server with the exception of packing the entire package which is for the deployment server.  Also installing of node modules is now done with 'pruning' to reduce the size of the package by 90%

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -4,19 +4,10 @@ curl --silent --location https://rpm.nodesource.com/setup_4.x | bash -
 yum -y install nodejs
 npm install -g npm
 npm install -g strong-pm
-npm install -g strongloop
-
-# Install Aurelia UI Dependencies.
-# We did not do this on the CI server because the bundle is ~300MB.
-# We will need to figure out a better way to handle this.
-cd client
-npm install -g gulp
-npm install
-gulp build
 
 # Install and Pack the Node/Loopback Dependencies
 # Unable to build and pack since CodeDeploy expects appspec.yml to be at the
 # root of the package and packing has an intermediate folder that disrupts.
-cd ..
-slc build --npm
+npm install -g strongloop
+slc build --pack
 sl-pm-install --upstart=0.6 --force
