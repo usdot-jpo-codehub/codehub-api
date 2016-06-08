ce285a09f0b8e72d488cff1a23a92568088f7ed5
natesol-code21 <ddeemeks@gmail.com>
2016-06-08 16:15:50 -0400

    #10 i) binding the top navigation to serve dynamically from the router configuration ii) adjusting the router to navigate popular projects, all projects & favorite projects iii) created a module for favorite projects

--- client/src/app.js
+++ client/src/app.js
@@ -2,12 +2,10 @@ export class App {
   configureRouter(config, router) {
     config.title = 'Aurelia';
     config.map([
-      { route: ['', 'project','stage'], name: 'index',      moduleId: 'modules/project/list',      nav: true, title: 'Welcome To Stage' },
-      { route: 'projects', name: 'projects',      moduleId: 'modules/project/list-all-projects',      nav: true, title: 'Welcome To Stage All Projects' },
-      { route: 'users',         name: 'users',        moduleId: 'users',        nav: true, title: 'Github Users' },
-      { route: 'child-router',  name: 'child-router', moduleId: 'child-router', nav: true, title: 'Child Router' }
+      { route: 'stage', name: 'project',      moduleId: 'modules/project/list',      nav: true, title: 'Feature Projects' },
+      { route: 'stage/projects', name: 'projects',      moduleId: 'modules/project/list-all-projects',      nav: true, title: 'Browse' },
+      { route: 'stage/favorites', name: 'favorites',      moduleId: 'modules/project/list-favorites',      nav: true, title: 'Favorites' },
     ]);
-
     this.router = router;
   }
 }
