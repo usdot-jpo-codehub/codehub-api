957b04fd9de3b551778df37c89eafd393882ebcb
Joshua Bock <Josh@JoshuaBock.com>
2016-06-13 13:38:55 -0400

    #37 Changed the text Featured Projects to Popular and the text Browse to Explorer.

--- client/src/app.js
+++ client/src/app.js
@@ -3,8 +3,8 @@ export class App {
     config.title = 'Stage';
     config.options.pushState = true;
     config.map([
-      { route: '', name: 'project',  moduleId: 'modules/project/popular/list',   nav: true,  title: 'Feature Projects' },
-      { route: 'projects', name: 'projects',      moduleId: 'modules/project/all-projects/list-all-projects',   nav: true, title: 'Browse' },
+      { route: '', name: 'project',  moduleId: 'modules/project/popular/list',   nav: true,  title: 'Popular' },
+      { route: 'projects', name: 'projects',      moduleId: 'modules/project/all-projects/list-all-projects',   nav: true, title: 'Explore' },
       { route: 'favorites', name: 'favorites',      moduleId: 'modules/project/favorites/list-favorites',  nav: true, title: 'Favorites' },
       { route: 'result',      name: 'result',     moduleId: 'modules/project/popular/result', nav: false},
       { route: 'result-all',      name: 'result-all',     moduleId: 'modules/project/all-projects/result-all', nav: false},
