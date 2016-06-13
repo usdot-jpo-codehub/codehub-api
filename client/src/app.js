a20746b0cbf87d8961ec7faff6e27d749299164d
natesol-code21 <ddeemeks@gmail.com>
2016-06-13 02:26:39 -0400

    #9 re-organizing and house keeping

--- client/src/app.js
+++ client/src/app.js
@@ -3,12 +3,12 @@ export class App {
     config.title = 'Stage';
     config.options.pushState = true;
     config.map([
-      { route: '', name: 'project',  moduleId: 'modules/project/list',   nav: true,  title: 'Feature Projects' },
-      { route: 'projects', name: 'projects',      moduleId: 'modules/project/list-all-projects',   nav: true, title: 'Browse' },
-      { route: 'favorites', name: 'favorites',      moduleId: 'modules/project/list-favorites',  nav: true, title: 'Favorites' },
-      { route: 'result',      name: 'result',     moduleId: 'modules/project/result', nav: false},
-      { route: 'result-all',      name: 'result-all',     moduleId: 'modules/project/result-all', nav: false},
-      { route: 'result-favorites',      name: 'result-favorites',     moduleId: 'modules/project/result-favorites', nav: false}
+      { route: '', name: 'project',  moduleId: 'modules/project/popular/list',   nav: true,  title: 'Feature Projects' },
+      { route: 'projects', name: 'projects',      moduleId: 'modules/project/all-projects/list-all-projects',   nav: true, title: 'Browse' },
+      { route: 'favorites', name: 'favorites',      moduleId: 'modules/project/favorites/list-favorites',  nav: true, title: 'Favorites' },
+      { route: 'result',      name: 'result',     moduleId: 'modules/project/popular/result', nav: false},
+      { route: 'result-all',      name: 'result-all',     moduleId: 'modules/project/all-projects/result-all', nav: false},
+      { route: 'result-favorites',      name: 'result-favorites',     moduleId: 'modules/project/favorites/result-favorites', nav: false}
     ]);
     this.router = router;
   }
