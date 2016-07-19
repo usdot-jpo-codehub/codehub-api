4e1ddbe2da8c6f810859b30a9cb36a761ec45868
natesol-code21 <ddeemeks@gmail.com>
2016-07-19 14:13:51 -0400

    #82 Project-Heimdall/home#14 Project-Heimdall/home#6 clean up to merge it with the master

--- common/models/project.js
+++ common/models/project.js
@@ -1,19 +1,3 @@
-/*
-
-  Operation Hooks: https://docs.strongloop.com/display/public/LB/Operation+hooks
-
-  Project is a PersistentModel working off a DB. Both Operation and Remote hooks work.
-
- */
-
 module.exports = function(Project) {
-  Project.observe("access", function(ctx, next) {
-    console.log('Project access called...');
-    next();
-  });
 
-  Project.beforeRemote("find", function(ctx, Project, next) {
-    console.log('Project find called...');
-    next();
-  });
 };
--- common/models/remote-project.js
+++ common/models/remote-project.js
@@ -1,25 +1,31 @@
 module.exports = function(RemoteProject) {
 
-   RemoteProject.afterRemote('searchPopular', function(ctx, RemoteProject, next) {
+   RemoteProject.afterRemote('find', function(ctx, RemoteProject, next) {
      var projs = [];
      for(proj of ctx.result.hits.hits){
-       projs.push({"name":proj._source.name,"organization":proj._source.organization,"full_name":proj._source.full_name,
-       "description":proj._source.description,"forks":proj._source.forks,"commits":proj._source.commits,
-       "email":proj._source.email, "type":proj._source.type, "language":proj._source.language,"html_url":proj._source.html_url,
-       "readme_url":proj._source.readme_url,"watchers":proj._source.watchers,"releases":proj._source.releases,
-       "total_score":proj._source._score,"type":proj._type,"id":proj._id});
+       projs.push(
+         {
+           "project_name":proj._source.project_name,
+           "organization":proj._source.organization,
+           "full_name":proj._source.full_name,
+           "project_description":proj._source.project_description,
+           "repository":proj._source.repository,
+           "forks":proj._source.forks,
+           "commits":proj._source.commits,
+           "email":proj._source.email,
+           "language":proj._source.language,
+           "content":proj._source.content,
+           "readme_url":proj._source.readme_url,
+           "contributors":proj._source.contributors,
+           "watchers":proj._source.watchers,
+           "releases":proj._source.releases,
+           "rank":proj._source.rank,
+           "id":proj._id
+         });
      }
      ctx.result = {
        projects: projs
      };
      next();
    });
-
-  RemoteProject.remoteMethod(
-    'searchPopular',
-    {
-      http: {path: '/', verb: 'get'},
-      returns: {arg: 'projects', type: 'string'}
-    }
-  );
 };
