ab4468fea65f54adf1e928e708f160aa77b31d89
Johnny Mohseni <jmohseni@gmail.com>
2016-08-21 09:23:15 -0400

    #108 updating IF conditionals to test for program existence and to install them if not found.  The prior logic was incorrect.

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -2,21 +2,34 @@
 
 cd /opt/heimdall
 
-if  [ ! hash node 2>/dev/null ] &&
-    [ ! hash slc 2>/dev/null ] &&
-    [ ! hash sl-pm-install 2>/dev/null ] &&
-    [ ! hash sl-pmctl 2>/dev/null ]
+# If node and its related parts are not installed then install them
+hash node 2>/dev/null
+if [ $? != 0 ]
 then
   yum -y install gcc-c++
   curl --silent --location https://rpm.nodesource.com/setup_4.x | bash -
   yum -y install nodejs
   npm install -g npm
-  npm install -g strong-pm
+fi
 
+hash slc 2>/dev/null
+if [ $? != 0 ]
+then
   # Install and Pack the Node/Loopback Dependencies
   # Unable to build and pack since CodeDeploy expects appspec.yml to be at the
   # root of the package and packing has an intermediate folder that disrupts.
   npm install -g strongloop
+fi
+
+hash sl-pm-install 2>/dev/null
+SLPMINSTALL=$?
+
+hash sl-pmctl 2>/dev/null
+SLPMCTL=$?
+
+if [ $SLPMINSTALL != 0 ] || [ $SLPMCTL != 0 ]
+then
+  npm install -g strong-pm
 fi
 
 slc build --pack
