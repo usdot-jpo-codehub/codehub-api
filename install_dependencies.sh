42c9d54bd7a3569c1f69018a52db27320d1d303d
Johnny Mohseni <jmohseni@gmail.com>
2016-08-20 22:58:02 -0400

    #108 adding more logic to the scripts so they do not keep adding services, basically a bit smarter

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,13 +1,24 @@
-cd /opt/heimdall
-yum -y install gcc-c++
-curl --silent --location https://rpm.nodesource.com/setup_4.x | bash -
-yum -y install nodejs
-npm install -g npm
-npm install -g strong-pm
+#!/bin/bash
 
-# Install and Pack the Node/Loopback Dependencies
-# Unable to build and pack since CodeDeploy expects appspec.yml to be at the
-# root of the package and packing has an intermediate folder that disrupts.
-npm install -g strongloop
+cd /opt/heimdall
+
+if  [ ! hash node 2>/dev/null ] &&
+    [ ! hash slc 2>/dev/null ] &&
+    [ ! hash sl-pm-install 2>/dev/null ] &&
+    [ ! hash sl-pmctl 2>/dev/null ]
+then
+  yum -y install gcc-c++
+  curl --silent --location https://rpm.nodesource.com/setup_4.x | bash -
+  yum -y install nodejs
+  npm install -g npm
+  npm install -g strong-pm
+
+  # Install and Pack the Node/Loopback Dependencies
+  # Unable to build and pack since CodeDeploy expects appspec.yml to be at the
+  # root of the package and packing has an intermediate folder that disrupts.
+  npm install -g strongloop
+fi
+
 slc build --pack
 sl-pm-install --upstart=0.6 --force
+
